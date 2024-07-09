38e17d857a114b882ba2122f8fc6b75bfbbfeef4
Asha <ashaa140922@gmail.com>
2024-07-09 11:48:40 +0530

    additional final_keyword Commit

--- src/final_keyword/final_method.java
+++ src/final_keyword/final_method.java
@@ -10,9 +10,12 @@ class Honda extends final_method{
   /*  void run(){  --------- cannot be overridden
         System.out.println("running safe");
     }*/
-
+void run(int i){ // can be overloaded
+    System.out.println("overloaded: " +i);
+}
     public static void main(String[] args){
         Honda h = new Honda();
         h.run();
+        h.run(8);
     }
 }
--- src/final_keyword/final_variable.java
+++ src/final_keyword/final_variable.java
@@ -1,13 +1,33 @@
 package final_keyword;
 
 public class final_variable {
-    final int speedlimit = 80; //final variable
+
+    // final int xyz= 90; -- cannot be changed even in constructor
+    final int speedlimit; //final variable - forced to initialize in constructor, once initialized, value cannot be changed
+
+    public final_variable(){
+        speedlimit = 80;
+    }
+
+    public final_variable(int speedlimit){
+        this.speedlimit = speedlimit;
+    }
+
     void run(){
         //speedlimit = 400; //Cannot assign a value to final variable 'speedlimit'
     }
 
+   // public static final double PI = 3.14;
+    public static double PI = 3.14;
     public static void main(String[] args){
         final_variable obj = new final_variable();
         obj.run();
+
+        final String s="Hello";
+        System.out.println(s);
+       // s="Hi"; -- Cannot assign a value to final variable 's'
+
+        PI = 2.56;
+        System.out.println(PI);
     }
 }
